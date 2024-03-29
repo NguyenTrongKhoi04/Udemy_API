8863a1a78b186a20d7dd6fc9c73d90e84e38f979
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-03-29 22:35:19 +0700

    29/03/2024

---
+++ Learning_Udemy/Phan_4_290302024_HTTP/10_Introduction to HTTP/script.js
@@ -0,0 +1,13 @@
+/**
+ * ? HTTP là gì
+ *      * là 1 giao thức, viết tắt của (Hyper Text Transfer Protocol)
+ *      * Dịch nôm na là giao thức truyền tải siêu văn bản, đc sử dụng trong "wwww"
+ * ? request/response HTTP bao gồm những gì ?
+ *      * gồm 4 phần: start line/header/blank line/body
+ *      * trong đó "start line" bắt buộc phải có
+ *      * cả request và response HTTP đều có 4 cái trên
+ */
+
+/**
+ * 
+ */
