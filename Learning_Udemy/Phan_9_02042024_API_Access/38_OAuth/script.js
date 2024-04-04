87969a0b934e522937995eff4d52c6ef7408cb09
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-04-04 20:57:03 +0700

    04/04/2024

---
+++ Learning_Udemy/Phan_9_02042024_API_Access/38_OAuth/script.js
@@ -0,0 +1,9 @@
+/**
+ * * Hiểu đơn giản là cấp quyền truy cập cho ứng dụng/web khác
+ *      ?  Nếu bạn đã có tài khoản fb thì đăng nhập vào ins ko cần login
+ *      ? phần bạn bè bản thân có thể để 3 chế độ khác nhau (public, private, public with friend)
+ */
+
+/**
+ * TODO Cơ chế hoạt động đang upate
+ */
